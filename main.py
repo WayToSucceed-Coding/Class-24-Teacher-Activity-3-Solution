75813905c25eae3ada173fc4ef75aef2a58c8105
WayToSucceed-Coding <aastha01@waytosucceedclasses.com>
2025-01-24 13:24:09 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,13 @@
+import random
+import time
+
+# n= random.randint(1,6)
+# print(n)
+
+print('Hello')
+time.sleep(5)
+print('I took a rest of 5 seconds.')
+
+devices=['Laptop','Smartphone','Speaker']
+print(random.choice(devices))
+
